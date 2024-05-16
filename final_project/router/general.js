6eedb02e5731c12462197daeeef9bb9bde893288
Tan Doan <tandoan@kms-technology.com>
2024-05-16 11:48:31 -0400

    10to14

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,6 +2,7 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+const axios = require('axios');
 const public_users = express.Router();
 
 
@@ -23,10 +24,28 @@ public_users.get('/',function (req, res) {
     res.send(JSON.stringify(books));
 });
 
+public_users.get('/async/', async function (req, res) {
+    try {
+        let response = await axios.get('https://tandoan-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/');
+        res.send(JSON.stringify(response.data));
+    } catch (error) {
+        res.status(500).json({message: "Internal error"});
+    }
+});
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   res.send(JSON.stringify(books[req.params.isbn]));
  });
+
+public_users.get('/async/isbn/:isbn', async function (req, res) {
+    try {
+        let response = await axios.get('https://tandoan-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/isbn/' + req.params.isbn);
+        res.send(JSON.stringify(response.data));
+    } catch (error) {
+        res.status(500).json({message: "Internal error"});
+    }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -35,11 +54,29 @@ public_users.get('/author/:author',function (req, res) {
   res.send(JSON.stringify(bookArr.filter(b => b.author == req.params.author)));
 });
 
+public_users.get('/async/author/:author', async function (req, res) {
+    try {
+        let response = await axios.get('https://tandoan-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/author/' + req.params.author);
+        res.send(JSON.stringify(response.data));
+    } catch (error) {
+        res.status(500).json({message: "Internal error"});
+    }
+});
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     let keys = Object.keys(books);
     let bookArr = keys.map(k => books[k]);
   res.send(JSON.stringify(bookArr.filter(b => b.title == req.params.title)));
+});
+
+public_users.get('/async/title/:title', async function (req, res) {
+    try {
+        let response = await axios.get('https://tandoan-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/title/' + req.params.title);
+        res.send(JSON.stringify(response.data));
+    } catch (error) {
+        res.status(500).json({message: "Internal error"});
+    }
 });
 
 //  Get book review
